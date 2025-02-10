fb89b60bc9ac70997c7b79172ba7e3740e4c7492
Radoslaw Hulboj <radekhulboj@gmail.com>
2025-02-10 18:43:41 +0100

    mobile devices are working fine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,33 +7,36 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
   standalone: true,
   imports: [
     RouterModule,
-    TranslateModule, // Import modułu tłumaczeń
+    TranslateModule,
   ],
   template: `
   <main>
     <section class="container">
       <header class="brand-name">
-        <a [routerLink]="['/']">
-          <img class="brand-logo" src="/assets/ajur1.svg" alt="logo" aria-hidden="true">
-          <img class="brand-logo" src="/assets/ajur2.svg" alt="logo" aria-hidden="true">
-          <img class="brand-logo" src="/assets/ajur3.svg" alt="logo" aria-hidden="true">
-          <img class="brand-logo" src="/assets/ajur4.svg" alt="logo" aria-hidden="true">
-          <img class="brand-logo" src="/assets/ajur5.svg" alt="logo" aria-hidden="true">
-          <img class="brand-logo" src="/assets/ajur6.svg" alt="logo" aria-hidden="true">
-          <img class="brand-logo" src="/assets/ajur7.svg" alt="logo" aria-hidden="true">
-        </a>
-        <nav class="menu">
-          <a [routerLink]="['/']" class="menu-item" routerLinkActive="active" [routerLinkActiveOptions]="{exact: true}">{{ 'MENU.HOME' | translate }}</a>
-          <a [routerLink]="['/events']" class="menu-item" routerLinkActive="active">{{ 'MENU.EVENTS' | translate }}</a>
-          <a [routerLink]="['/editor']" class="menu-item" routerLinkActive="active">{{ 'MENU.EDITOR' | translate }}</a>
-          <a [routerLink]="['/contact']" class="menu-item" routerLinkActive="active">{{ 'MENU.CONTACT' | translate }}</a>
+      <a [routerLink]="['/']">
+        <img class="brand-logo" src="/assets/ajur1.svg" alt="logo" aria-hidden="true">
+        <img class="brand-logo" src="/assets/ajur2.svg" alt="logo" aria-hidden="true">
+        <img class="brand-logo" src="/assets/ajur3.svg" alt="logo" aria-hidden="true">
+        <img class="brand-logo" src="/assets/ajur4.svg" alt="logo" aria-hidden="true">
+        <img class="brand-logo" src="/assets/ajur5.svg" alt="logo" aria-hidden="true">
+        <img class="brand-logo" src="/assets/ajur6.svg" alt="logo" aria-hidden="true">
+        <img class="brand-logo" src="/assets/ajur7.svg" alt="logo" aria-hidden="true">
+      </a>
+        <button class="menu-toggle" (click)="toggleMenu()">
+          ☰
+        </button>
+       <!-- Menu -->
+        <nav [class.open]="menuOpen" class="menu">
+          <a [routerLink]="['/']" class="menu-item" routerLinkActive="active" [routerLinkActiveOptions]="{exact: true}" (click)="closeMenu()">{{ 'MENU.HOME' | translate }}</a>
+          <a [routerLink]="['/events']" class="menu-item" routerLinkActive="active" (click)="closeMenu()">{{ 'MENU.EVENTS' | translate }}</a>
+          <a [routerLink]="['/editor']" class="menu-item" routerLinkActive="active" (click)="closeMenu()">{{ 'MENU.EDITOR' | translate }}</a>
+          <a [routerLink]="['/contact']" class="menu-item" routerLinkActive="active" (click)="closeMenu()">{{ 'MENU.CONTACT' | translate }}</a>
         </nav>
 
         <div class="language-selector">
           <button [class.active]="currentLang === 'en'" (click)="changeLanguage('en')">En</button>
           <button [class.active]="currentLang === 'pl'" (click)="changeLanguage('pl')">Pl</button>
         </div>
-
       </header>
     </section>
     <section class="content">
@@ -45,21 +48,27 @@ import { TranslateModule, TranslateService } from '@ngx-translate/core';
 })
 export class AppComponent {
   title = 'tata radek';
-  currentLang: string = 'en'; // Domyślny język
+  currentLang: string = 'en';
+  menuOpen = false;
 
   constructor(private translate: TranslateService) {
-    this.translate.setDefaultLang('en'); // Domyślny język
-    this.translate.use('en'); // Aktywny język
-  }
-
-  ngOnInit() {
-    this.adjustContentPadding();
+    this.translate.setDefaultLang('en');
+    this.translate.use('en');
   }
 
   changeLanguage(lang: string) {
-    this.translate.use(lang); // Zmiana języka
-    this.currentLang = lang; // Ustawienie aktywnego języka
+    this.translate.use(lang);
+    this.currentLang = lang;
   }
+
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  closeMenu() {
+    this.menuOpen = false; // Zamyka menu po kliknięciu
+  }
+
 
   @HostListener('window:resize')
   adjustContentPadding() {
@@ -67,12 +76,7 @@ export class AppComponent {
     const content = document.querySelector('section.content');
     if (header && content) {
       const headerHeight = header.offsetHeight;
-      // Obliczamy sumę headerHeight i 20px
-      const adjustedHeight = headerHeight + 20;
-      (content as HTMLElement).style.marginTop = `${adjustedHeight}px`;
+      (content as HTMLElement).style.marginTop = `${headerHeight + 20}px`;
     }
   }
-  
-  
-
 }
