20684726837c042119c676cddeaee0f3c255645c
Radoslaw Hulboj <radekhulboj@gmail.com>
2025-07-12 13:07:20 +0200

    proxy comments

--- proxy.conf.js
+++ proxy.conf.js
@@ -1,5 +1,7 @@
-// const target = process.env.API_TARGET || "http://ayurveda-server-service:8080";
-const target = process.env.API_TARGET || "http://localhost:8080";
+ const target = process.env.API_TARGET || "http://ayurveda-server-service:8080";  // kubernetes
+// const target = process.env.API_TARGET || "http://localhost:8080";                   // for local execution
+// const target = process.env.API_TARGET || "http://ayurveda-server:8080";          // for docker-compose-springboot 
+
 
 module.exports = {
   "/api": {
