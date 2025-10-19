b4cc199d4cd4dedfc867c8ee8d3474fd27ca1937
Radoslaw Hulboj <radekhulboj@gmail.com>
2025-10-19 21:36:07 +0200

    production preparing - local works too

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,15 +1,4 @@
 export const environment = {
     production: false,
-    // to jest dla Azure Container Apps
-    //  apiUrl: 'https://ayurveda-server.redrock-11e93e23.polandcentral.azurecontainerapps.io/api'
-
-
-    // a to nie wiem co robi, moze dla ingress - trzeba sprawdzic
-    apiUrl: '/api'
-
-    // To jest dla Kubernetes dla service level (ale wydaje mi sie ze wystarczy po mojej zmianie uzywac API_TARGET, 
-    // tylko to jest zle rozwiazanie bo uzywam dev env w kubernetes)
-    // apiUrl: 'http://ayurveda-server-service:8080/api
-    
-  };
-  
+    apiUrl: 'http://localhost:8080/api'
+};
